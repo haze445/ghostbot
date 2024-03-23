05183b752dd8a39ce38fb7674dc9d02995523913
haze445 <164426563+haze445@users.noreply.github.com>
2024-03-23 23:17:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Haze Family","Watching Haze Family"];
 
 
 let currentIndex = 0;
